c40ea27b2d80c281d2918c945927ff82d5646acd
BigestCola <jiuhualai@gmail.com>
2024-05-19 21:15:44 +0800

    automation

--- FristTimeUploadToGithub.bat
+++ FristTimeUploadToGithub.bat
@@ -1,7 +1,7 @@
 git config --global user.email "jiuhualai@gmail.com"
 git config --global user.name "BigestCola"
 git init
-git commit -m "first commit"
+git commit -m "automation"
 git remote add origin https://github.com/BigestCola/automation
 git add . 
 git push -u origin master
